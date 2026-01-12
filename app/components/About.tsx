8736f30fee47f4631c43643616143c37f0b27bca
Drew <chukwukaenudeme@gmail.com>
2026-01-12 17:41:04 +0100

    made some changes

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -8,8 +8,8 @@ const About = () => {
       <div className="container">
         <div className=" bg-zinc-800/50 p-7  rounded-2xl md:p-12">
           <p className=" text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch] ">
-            Welcome! I&apos;m Chukwuka, a professional web developer with a
-            knack for creafting visually stunning and highly functional
+            Welcome! I&apos;m Chukwuka, a professional Frontend web developer with a
+            zeal for creafting visually stunning and highly functional
             websites. Combining creativity and technical expertise. I transform
             your vision into digital masterpiece that excels in both appearance
             and performance
